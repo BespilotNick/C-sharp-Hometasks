f645dced2d4160de5e868287bdb1f357377505cf
NickAllenov <AllenovNS@gmail.com>
2022-04-21 23:57:35 +0300

    task03

---
+++ hometask001-03/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if((number % 2) == 0)
+    Console.WriteLine("Your integer is Even");
+else
+    Console.WriteLine("Your integer is Odd");
