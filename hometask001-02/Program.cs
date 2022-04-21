8b1a7a91c2431bf46a1b25599071cdfc82ff9ed8
NickAllenov <AllenovNS@gmail.com>
2022-04-22 00:41:23 +0300

    docstrings added

--- hometask001-02/Program.cs
+++ hometask001-02/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.Write("Input first integer number:  ");
+﻿/*
+Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Input first integer number:  ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second integer number:  ");
 int number2 = Convert.ToInt32(Console.ReadLine());
--- hometask001-03/Program.cs
+++ hometask001-03/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.Write("input integer number: ");
+﻿/*
+Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("input integer number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 if((number % 2) == 0)
