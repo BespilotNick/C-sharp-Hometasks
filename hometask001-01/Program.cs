e3156ff488a274ded1625689f730b5848ec9c801
NickAllenov <AllenovNS@gmail.com>
2022-04-21 23:37:42 +0300

    task01 + gitignore

---
+++ hometask001-01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Input first integer number:  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number:  ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+    Console.WriteLine("Max number is " + number1);
+else
+    Console.WriteLine("Max number is " + number2);
