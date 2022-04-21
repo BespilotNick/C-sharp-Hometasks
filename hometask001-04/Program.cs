33a45d73f3c9ed068b2288746766b3e5f46792ac
NickAllenov <AllenovNS@gmail.com>
2022-04-22 00:29:43 +0300

    task04

---
+++ hometask001-04/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+if(number > 0)
+    {
+        while(current <= number)
+        {
+            if((current % 2) == 0)
+                Console.Write(current + "  ");
+                current++;
+        }
+    }
+
+if(number < 0)
+    {
+        while(current >= number)
+            {
+                if((current % 2) == 0)
+                    Console.Write(current + "  ");
+                    current--;
+            }
+    }
