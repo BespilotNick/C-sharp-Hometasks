e101a33c43f97f14b351d31ac13776fd06047c1d
NickAllenov <AllenovNS@gmail.com>
2022-04-25 00:11:27 +0300

    Is a day a weekend

---
+++ hometask002-02(task13)/Program.cs
@@ -0,0 +1,18 @@
+﻿// The program that prints the third digit of a number (if it exists)
+
+void ThirdNum(int[] array)
+{
+    if(array.Length > 2)
+    {
+        Console.WriteLine("The third number of " + array + " is " + array[2]);
+    }
+    else
+    {
+        Console.WriteLine("The third number is not exists");
+    }
+}
+
+Console.WriteLine("Input you number: ");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+userarray.ToBase64CharArray;
+ThirdNum(userarray);
